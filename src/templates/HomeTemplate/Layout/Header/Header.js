af4a2b143ff43a8e72e342a92307f1b56f4eb6f3
Nhat Pham <pmnhat.17ck1@gmail.com>
2021-10-13 00:38:06 +0700

    add active header

--- src/templates/HomeTemplate/Layout/Header/Header.js
+++ src/templates/HomeTemplate/Layout/Header/Header.js
@@ -29,6 +29,7 @@ export default function Header(props) {
   const { userLogin } = useSelector(state => state.QuanLyNguoiDungReducer);
 
   const [sidebar, setSidebar] = useState(false);
+  const [activepage, setActivePage] = useState(0);
 
   const showSidebar = () => setSidebar(!sidebar);
 
@@ -58,11 +59,36 @@ export default function Header(props) {
   return (
 
     <div ref={headerRef} className="header shrink">
-      <div className="header__wrap container">
+      <div className="header__wrap">
         <div className="logo">
 
           <Link to="/" className="text-white hover:text-yellow-400"><img src="https://cyberlearn.vn/wp-content/uploads/2020/03/cyberlearn-min-new-opt2.png" alt="cybersoft.edu.vn" /></Link>
         </div>
+        <ul className=" mt-5 mr-10 items-stretch hidden space-x-3 lg:flex">
+           <li className="flex">
+             <NavLink to="/" onClick={()=>{setActivePage(1)}}
+               className={`flex items-center  border-b-2 px-4 border-transparent text-black ${activepage===1 ? `text-yellow-400` : ''} hover:text-yellow-400 duration-300" activeClassName="border-b-2 border-white`}
+            >
+              Trang Chủ
+           </NavLink>
+          </li>
+          <li className="flex">
+            <a href="#phim" 
+              className={`flex items-center  border-b-2 px-4 border-transparent text-black ${activepage===2 ? `text-yellow-400` : ''} hover:text-yellow-400 duration-300" activeClassName="border-b-2 border-white`}
+              onClick={()=>{setActivePage(2)}}
+            >
+              Phim
+          </a>
+         </li>
+          <li className="flex">
+            <a href="#cumrap"
+              className={`flex items-center border-b-2 px-4 mr-10 border-transparent text-black  ${activepage===3 ? `text-yellow-400` : ''} hover:text-yellow-400 duration-300" activeClassName="border-b-2 border-white`}
+              onClick={()=>{setActivePage(3)}}
+           >
+              Cụm Rạp
+            </a>
+         </li>
+       </ul>
         <ul className="header__nav">
 
           <li className={pathname==='/' ? 'active' : ''}>
@@ -80,7 +106,6 @@ export default function Header(props) {
               <UserOutlined />
             </Link>
           </li>)}
-
         </ul>
       </div>
     </div>
